f09790331d82f8f938b2d73c40cefe822812a024
Pfoof <rotworm0@yahoo.pl>
2018-09-20 16:30:45 +0200

    Added timeout for Modules in CheckRunnable. Can be also set to a different value. Sets status to STATUS_TIMEOUT. Happens only when prior status of the module is STATUS_WAITING.

--- Manager/src/main/java/io/raspberrywallet/manager/modules/Module.java
+++ Manager/src/main/java/io/raspberrywallet/manager/modules/Module.java
@@ -1,5 +1,7 @@
 package io.raspberrywallet.manager.modules;
 
+import org.omg.CORBA.TIMEOUT;
+
 public abstract class Module {
 
     /**
@@ -77,9 +79,11 @@ public abstract class Module {
     }
 
     public static final int STATUS_OK = 200;
+    public static final int STATUS_TIMEOUT = 432;
+    public static final int STATUS_WAITING = 100;
 
     private byte[] payload;
-    private int status;
+    private int status = STATUS_WAITING;
     private String statusString;
     private byte[] decryptedValue;
 
@@ -114,6 +118,8 @@ public abstract class Module {
 
         private boolean run = false;
         private long sleepTime = 1000;
+        private long timeout = 3000;
+        private long startTime;
 
         public CheckRunnable stop() {
             run = false;
@@ -130,14 +136,28 @@ public abstract class Module {
             return this;
         }
 
+        public CheckRunnable setTimeout(long tout) {
+            timeout = tout;
+            return this;
+        }
+
         public void run() {
+            startTime = System.currentTimeMillis();
             while (run) {
+
                 if (check())
                     process();
+
+                if(System.currentTimeMillis() - startTime > timeout && status == STATUS_WAITING) {
+                    run = false;
+                    status = STATUS_TIMEOUT;
+                    statusString = "Timed out waiting for Module interaction.";
+                }
             }
             try {
                 Thread.sleep(sleepTime);
             } catch (Exception e) {
+                e.printStackTrace();
             }
         }
     }
@@ -146,13 +166,14 @@ public abstract class Module {
 
     //Co ma się stać po wykonaniu wszystkiego, czyli odblokowaniu klucza lub anuluj
     public void destroy() {
+
         //Kończymy oczekiwanie
         checkRunnable.stop();
         try {
             //Join bo tak ładnie
             checkThread.join(checkRunnable.sleepTime * 2);
         } catch (Exception e) {
-
+            e.printStackTrace();
         } finally {
             //Zerujemy pamięć
             synchronized (this) {
