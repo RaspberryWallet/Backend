a3524ed56043c9daf8f046509fcef121c760b024
PatrykMilewski <patryk.milewski@gmail.com>
2018-10-01 20:50:37 +0200

    Added Stasbar suggestions from PR#27

--- Manager/src/main/java/io/raspberrywallet/manager/linux/TemperatureMonitor.java
+++ Manager/src/main/java/io/raspberrywallet/manager/linux/TemperatureMonitor.java
@@ -7,6 +7,8 @@ import java.util.regex.Pattern;
 
 public class TemperatureMonitor extends Executable {
 
+    private Pattern pattern = Pattern.compile("^[0-9]+$");
+    
     public TemperatureMonitor() {
         this.id = "435a06ba-b2b7-4de8-843c-f5f836c2a523";
     }
@@ -23,7 +25,7 @@ public class TemperatureMonitor extends Executable {
             Process process = Runtime.getRuntime().exec("cat /sys/class/thermal/thermal_zone0/temp");
             process.waitFor();
             
-            return getActualTemperature(process);
+            return parseTemperatureFrom(process);
             
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
@@ -31,9 +33,8 @@ public class TemperatureMonitor extends Executable {
         }
     }
     
-    private String getActualTemperature(Process process) throws IOException {
+    private String parseTemperatureFrom(Process process) throws IOException {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
-            Pattern pattern = Pattern.compile("^[0-9]+$");
             String line;
             while ((line = reader.readLine()) != null)
                 if (pattern.matcher(line).matches())
