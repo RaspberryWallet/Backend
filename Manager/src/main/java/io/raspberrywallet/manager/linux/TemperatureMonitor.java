d45f24b59e489861f1137c8abb6e2b2732c6318e
stasbar <stasbar1995@gmail.com>
2018-10-02 13:15:24 +0200

    fixed a bug where Manager parsing "undefined" would throw NumberFormatException fixed a bug where "undefined" would never be returned by Manager Manager should be blind on TemperatureMonitor implementation
    Took 15 minutes

--- Manager/src/main/java/io/raspberrywallet/manager/linux/TemperatureMonitor.java
+++ Manager/src/main/java/io/raspberrywallet/manager/linux/TemperatureMonitor.java
@@ -1,5 +1,7 @@
 package io.raspberrywallet.manager.linux;
 
+import io.raspberrywallet.manager.linux.exceptions.TemperatureNotFoundException;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -25,22 +27,25 @@ public class TemperatureMonitor extends Executable<String> {
             Process process = Runtime.getRuntime().exec("cat /sys/class/thermal/thermal_zone0/temp");
             process.waitFor();
 
-            return parseTemperatureFrom(process);
-
+            String parsedTemp = parseTemperatureFrom(process);
+            float value = Float.parseFloat(parsedTemp);
+            return String.format("%.3f °C", value / 1000);
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
             return "error";
+        } catch (TemperatureNotFoundException | NumberFormatException e) {
+            e.printStackTrace();
+            return "undefined";
         }
     }
 
-    private String parseTemperatureFrom(Process process) throws IOException {
+    private String parseTemperatureFrom(Process process) throws IOException, TemperatureNotFoundException {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
             String line;
             while ((line = reader.readLine()) != null)
                 if (pattern.matcher(line).matches())
                     return line;
-
-            return "undefined";
+            throw new TemperatureNotFoundException();
         }
     }
 }
