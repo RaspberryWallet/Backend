8b2a02b7613d817bceaa50ea1aed84b0d7872bd1
stasbar <stasbar1995@gmail.com>
2018-10-03 10:13:49 +0200

    ktor use Jetty
    Took 7 minutes

--- ServerHttp/src/main/kotlin/io/raspberrywallet/Main.kt
+++ ServerHttp/src/main/kotlin/io/raspberrywallet/Main.kt
@@ -2,14 +2,14 @@ package io.raspberrywallet
 
 import io.ktor.application.Application
 import io.ktor.server.engine.embeddedServer
-import io.ktor.server.netty.Netty
+import io.ktor.server.jetty.Jetty
 import io.raspberrywallet.ktor.mainModule
 import io.raspberrywallet.mock.ManagerMock
 import io.raspberrywallet.server.Server
 
 internal fun main(args: Array<String>) {
     if (args.isNotEmpty() && args[0] == "ktor") {
-        embeddedServer(Netty, Server.PORT, module = Application::mainModule).start(wait = true)
+        embeddedServer(Jetty, Server.PORT, module = Application::mainModule).start(wait = true)
     } else {
         val server = Server(ManagerMock())
         server.start()
