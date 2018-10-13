75498f358d37cc5f25ea94230ea8fd06e3350bcc
Pfoof <rotworm0@yahoo.pl>
2018-10-13 17:21:45 +0200

    Matching for current connection status.

--- Manager/src/main/java/io/raspberrywallet/manager/linux/WifiStatus.java
+++ Manager/src/main/java/io/raspberrywallet/manager/linux/WifiStatus.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class WifiStatus extends Executable<Map<String, String>> {
@@ -35,12 +36,36 @@ public class WifiStatus extends Executable<Map<String, String>> {
     public Map<String, String> call() throws Exception {
         HashMap<String, String> values = new HashMap<String, String>();
         try {
+
             Process process = Runtime.getRuntime().exec("iwconfig wlan0");
             process.waitFor();
             BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
             String line;
             while( (line = reader.readLine()) != null) {
+                Matcher essidMatcher = essidPattern.matcher(line);
+                Matcher macMatcher = macPattern.matcher(line);
+                Matcher freqMatcher = freqPattern.matcher(line);
+                Matcher qualityMatcher = qualityPattern.matcher(line);
+                Matcher speedMatcher = speedPattern.matcher(line);
+                if(essidMatcher.matches()) values.put("essid", essidMatcher.group(1));
+                if(macMatcher.matches()) values.put("mac", macMatcher.group(1));
+                if(freqMatcher.matches()) values.put("freq", freqMatcher.group(1));
+                if(qualityMatcher.matches()) values.put("quality", qualityMatcher.group(1));
+                if(speedMatcher.matches()) values.put("speed", speedMatcher.group(1));
+            }
 
+            process = Runtime.getRuntime().exec("ifconfig wlan0");
+            process.waitFor();
+            reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            line = null;
+            while( (line = reader.readLine()) != null) {
+                Matcher ipMatcher = ipPattern.matcher(line);
+                Matcher ipv6Matcher = ipv6Pattern.matcher(line);
+                Matcher globalipMatcher = globalipPattern.matcher(line);
+
+                if(ipMatcher.matches()) values.put("ip", ipMatcher.group(1));
+                if(ipv6Matcher.matches()) values.put("ipv6", ipv6Matcher.group(1));
+                if(globalipMatcher.matches()) values.put("globalip", globalipMatcher.group(1));
             }
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
