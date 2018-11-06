e3a676cb9a8f4ee0b9d3a789175ca504503026b7
stasbar <stasbar1995@gmail.com>
2018-11-06 15:44:24 +0100

    WalletNotInitialized handled error
    Took 9 minutes

--- ServerHttp/src/main/kotlin/io/raspberrywallet/server/KtorServer.kt
+++ ServerHttp/src/main/kotlin/io/raspberrywallet/server/KtorServer.kt
@@ -9,6 +9,7 @@ import io.ktor.application.install
 import io.ktor.features.CallLogging
 import io.ktor.features.ContentNegotiation
 import io.ktor.features.DefaultHeaders
+import io.ktor.features.StatusPages
 import io.ktor.html.HtmlContent
 import io.ktor.http.HttpStatusCode
 import io.ktor.http.content.resources
@@ -25,6 +26,7 @@ import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import io.raspberrywallet.contract.Manager
+import io.raspberrywallet.contract.WalletNotInitialized
 import io.raspberrywallet.server.Paths.Bitcoin.availableBalance
 import io.raspberrywallet.server.Paths.Bitcoin.currentAddress
 import io.raspberrywallet.server.Paths.Bitcoin.estimatedBalance
@@ -105,6 +107,11 @@ fun Application.mainModule() {
         level = Level.INFO
     }
     install(DefaultHeaders)
+    install(StatusPages) {
+        exception<WalletNotInitialized> { cause ->
+            call.respond(HttpStatusCode.MethodNotAllowed, "Wallet not initialized")
+        }
+    }
 
     routing {
         /* Index */
