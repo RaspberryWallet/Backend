d81abad09fbb50f8e05cd97098a12e8c6cdf2cca
Patryk Milewski <patryk.milewski@gmail.com>
2018-11-10 17:39:59 +0100

    Added HTTPS support for authorization server module
    Refactor of Apache Http client wrappers
    
    Took 27 minutes

--- Manager/src/main/java/io/raspberrywallet/manager/common/http/ApacheHttpClient.java
+++ Manager/src/main/java/io/raspberrywallet/manager/common/http/ApacheHttpClient.java
@@ -1,24 +1,63 @@
 package io.raspberrywallet.manager.common.http;
 
+import org.apache.commons.lang.NotImplementedException;
+import org.apache.http.Header;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.fluent.Form;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicHeader;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public abstract class ApacheHttpClient {
-
-    public abstract HttpResponse sendPOSTRequest(Form body, String endpoint) throws IOException;
     
-    public abstract HttpResponse sendGETRequest(Form body, String endpoint);
+    Header[] defaultHeaders;
+    HttpClient httpClient;
+    
+    ApacheHttpClient(Form defaultHeaders) {
+        this.defaultHeaders = toHeadersArray(defaultHeaders);
+        httpClient = HttpClients.createDefault();
+    }
+    
+    public HttpResponse sendPOSTRequest(Form body, String endpoint) throws IOException {
+        JSONObject jsonObject = convert(body);
+        StringEntity stringEntity = new StringEntity(jsonObject.toString());
+    
+        HttpPost httpRequest = new HttpPost(endpoint);
+        httpRequest.setEntity(stringEntity);
+        httpRequest.setHeaders(defaultHeaders);
+    
+        return httpClient.execute(httpRequest);
+    }
+    
+    public HttpResponse sendGETRequest(Form body, String endpoint) {
+        throw new NotImplementedException();
+    }
     
     JSONObject convert(Form body) {
         List<NameValuePair> keyValuesList = body.build();
         JSONObject convertedJson = new JSONObject();
         keyValuesList.forEach(pair -> convertedJson.put(pair.getName(), pair.getValue()));
         return convertedJson;
+    }
+    
+    Header[] toHeadersArray(Form form) {
+        List<NameValuePair> list = form.build();
+        List<Header> result = new ArrayList<>(list.size());
+        list.forEach(nameValuePair -> {
+            BasicHeader basicHeader = new BasicHeader(nameValuePair.getName(), nameValuePair.getValue());
+            result.add(basicHeader);
+        });
+        
+        Header[] headers = new Header[result.size()];
+        return result.toArray(headers);
     }
     
 }
--- Manager/src/main/java/io/raspberrywallet/manager/common/http/UnsecureApacheHttpClient.java
+++ Manager/src/main/java/io/raspberrywallet/manager/common/http/UnsecureApacheHttpClient.java
@@ -4,6 +4,7 @@ import org.apache.commons.lang.NotImplementedException;
 import org.apache.http.Header;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.fluent.Form;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
@@ -17,40 +18,10 @@ import java.util.List;
 
 public class UnsecureApacheHttpClient extends ApacheHttpClient {
     
-    private Header[] defaultHeaders;
+    private HttpClient client = HttpClients.createDefault();
     
     public UnsecureApacheHttpClient(Form defaultHeaders) {
-        this.defaultHeaders = toHeadersArray(defaultHeaders);
+        super(defaultHeaders);
     }
     
-    @Override
-    public HttpResponse sendPOSTRequest(Form body, String endpoint) throws IOException {
-        org.apache.http.client.HttpClient client = HttpClients.createDefault();
-        
-        JSONObject jsonObject = convert(body);
-        StringEntity stringEntity = new StringEntity(jsonObject.toString());
-        
-        HttpPost httpRequest = new HttpPost(endpoint);
-        httpRequest.setEntity(stringEntity);
-        httpRequest.setHeaders(defaultHeaders);
-        
-        return client.execute(httpRequest);
-    }
-    
-    @Override
-    public HttpResponse sendGETRequest(Form body, String endpoint) {
-        throw new NotImplementedException();
-    }
-    
-    private Header[] toHeadersArray(Form form) {
-        List<NameValuePair> list = form.build();
-        List<Header> result = new ArrayList<>(list.size());
-        list.forEach(nameValuePair -> {
-            BasicHeader basicHeader = new BasicHeader(nameValuePair.getName(), nameValuePair.getValue());
-            result.add(basicHeader);
-        });
-        
-        Header[] headers = new Header[result.size()];
-        return result.toArray(headers);
-    }
 }
