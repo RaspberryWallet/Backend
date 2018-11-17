7cb6b1c56c9b5015985cb596b7f387afcd838936
stasbar <stasbar1995@gmail.com>
2018-11-17 15:52:44 +0100

    fix buildAndCopyWebApp.py to not remove whole /asset dir, instead, override files add all files under git control
    Took 11 minutes

--- Scripts/webapp/buildAndCopyWebApp.py
+++ Scripts/webapp/buildAndCopyWebApp.py
@@ -7,6 +7,7 @@ import subprocess
 while not os.getcwd().lower().endswith("backend"):
     os.chdir("..")
 
-shutil.rmtree("ServerHttp/src/main/resources/assets")
 subprocess.call(['yarn', '--cwd', '../../JSProjects/raspberry-wallet-frontend/', 'run', 'build'])
-shutil.copytree('../../JSProjects/raspberry-wallet-frontend/build/', 'ServerHttp/src/main/resources/assets/')
+shutil.rmtree("ServerHttp/src/main/resources/assets/static/js")
+os.system("cp -rf ../../JSProjects/raspberry-wallet-frontend/build/ ServerHttp/src/main/resources/assets/")
+os.system("git add ServerHttp/src/main/resources/assets")
