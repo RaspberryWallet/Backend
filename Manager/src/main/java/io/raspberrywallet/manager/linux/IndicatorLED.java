d7c8b065980bae0707b66fc92a9d94a20dca9cbc
Pfoof <rotworm0@yahoo.pl>
2018-12-01 12:20:21 +0100

    Created an IndicatorLED on GPIO_05 (24 on pinout.xyz). Can be set to on, off or blink with adjustable times.

---
+++ Manager/src/main/java/io/raspberrywallet/manager/linux/IndicatorLED.java
@@ -0,0 +1,90 @@
+package io.raspberrywallet.manager.linux;
+
+import com.pi4j.io.gpio.*;
+import com.pi4j.wiringpi.Gpio;
+import lombok.Getter;
+import lombok.Setter;
+
+public class IndicatorLED {
+
+    private final Pin ledPin = RaspiPin.GPIO_05;
+    private final GpioController gpioController = GpioFactory.getInstance();
+    private GpioPinDigitalOutput ledOutput;
+
+    public IndicatorLED() {
+        ledOutput = gpioController.provisionDigitalOutputPin(ledPin);
+    }
+
+    private Thread blinkThread;
+    private BlinkRunnable blinkRunnable;
+
+    private class BlinkRunnable implements Runnable {
+
+        @Setter private int offMs = 50;
+        @Setter private int onMs = 50;
+        @Setter private boolean work = true;
+
+        public BlinkRunnable(int offMs, int onMs) {
+            setOffMs(offMs);
+            setOnMs(onMs);
+            setWork(true);
+        }
+
+        @Override
+        public void run() {
+            while(work) {
+
+                ledOutput.blink(offMs, onMs);
+                try {Thread.sleep(offMs + onMs);} catch(Exception e) {}
+
+            }
+        }
+
+    }
+
+    /**
+     * Set the indicator LED (GPIO_24) to ON.
+     */
+    public synchronized void on() {
+        stopLed();
+        ledOutput.high();
+    }
+
+    /**
+     * Set the indicator LED (GPIO_24) to OFF.
+     */
+    public synchronized void off() {
+        stopLed();
+    }
+
+    /**
+     * Starts blinking until off()/on() is called.
+     * @param offMs - how long should the led be dark in miliseconds
+     * @param onMs - how long should the led be bright in miliseconds
+     */
+    public synchronized void blink(int offMs, int onMs) {
+
+        if(blinkThread != null) {
+            stopLed();
+        }
+
+        blinkRunnable = new BlinkRunnable(offMs, onMs);
+        blinkThread = new Thread(blinkRunnable);
+        blinkThread.start();
+
+    }
+
+    private synchronized void stopLed() {
+        if(blinkThread != null) {
+            blinkRunnable.setWork(false);
+            try {
+                blinkThread.join();
+            } catch (Exception e) {
+
+            }
+            blinkThread = null;
+        }
+        ledOutput.low();
+    }
+
+}
