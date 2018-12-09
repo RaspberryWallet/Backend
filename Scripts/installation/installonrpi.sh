ec7804f90a4acb903a3136f1e008bfa18c04f501
stasbar <stasbar1995@gmail.com>
2018-12-09 04:01:59 +0100

    update installonrpi script to inform about applying changes
    Took 5 minutes

--- Scripts/installation/installonrpi.sh
+++ Scripts/installation/installonrpi.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-scp ../../Manager/target/*with-dependencies.jar dietpi@10.7.7.2:/home/dietpi/Manager.jar
-scp -r ../../modules dietpi@10.7.7.2:/home/dietpi/
-ssh dietpi@10.7.7.2 installwallet
+scp ../../Manager/target/*with-dependencies.jar dietpi@10.7.7.2:/home/dietpi/Manager.jar && \
+scp -r ../../modules dietpi@10.7.7.2:/home/dietpi/ && \
+ssh dietpi@10.7.7.2 "installwallet" && \
+echo "[ℹ] To apply the changes execute: dietpi@10.7.7.2 restart"
